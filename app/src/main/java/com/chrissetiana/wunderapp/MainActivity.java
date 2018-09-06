8af985d83a20afc032bbd84d77b88f4ac6de5fee
chrissetiana <chrissetiana@gmail.com>
2018-09-06 21:28:55 +0100

    Create layout

--- app/src/main/java/com/chrissetiana/wunderapp/MainActivity.java
+++ app/src/main/java/com/chrissetiana/wunderapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.chrissetiana.wunderapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listCars;
+    TextView textEmpty;
+    View progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listCars = findViewById(R.id.list_cars);
+        textEmpty = findViewById(R.id.text_empty);
+        progressBar = findViewById(R.id.progress_bar);
     }
 }
