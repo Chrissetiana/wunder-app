eabb3c6bdfb668df3010ffe089bb950cd3cedba5
chrissetiana <chrissetiana@gmail.com>
2018-09-06 22:48:23 +0100

    Connect to the internet

--- app/src/main/java/com/chrissetiana/wunderapp/MainActivity.java
+++ app/src/main/java/com/chrissetiana/wunderapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.chrissetiana.wunderapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -20,5 +23,23 @@ public class MainActivity extends AppCompatActivity {
         listCars = findViewById(R.id.list_cars);
         textEmpty = findViewById(R.id.text_empty);
         progressBar = findViewById(R.id.progress_bar);
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connectivityManager != null;
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(networkInfo != null && networkInfo.isConnected()) {
+            loadQuery();
+        } else {
+            listCars.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.INVISIBLE);
+            textEmpty.setText(getString(R.string.no_conn));
+        }
+    }
+
+    private void loadQuery() {
+        listCars.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+        textEmpty.setText("Connected.");
     }
 }
