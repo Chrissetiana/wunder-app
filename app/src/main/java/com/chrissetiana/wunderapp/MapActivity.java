c3df0416d14acc4a4b469e9b6c115ff918252022
chrissetiana <chrissetiana@gmail.com>
2018-09-10 20:21:33 +0100

    Modify map

--- app/src/main/java/com/chrissetiana/wunderapp/MapActivity.java
+++ app/src/main/java/com/chrissetiana/wunderapp/MapActivity.java
@@ -1,6 +1,7 @@
 package com.chrissetiana.wunderapp;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -13,6 +14,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -21,6 +23,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
 
         if (isPlayAvailable()) {
             setContentView(R.layout.activity_map);
@@ -47,15 +51,23 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    private void loadLocation(double lat, double lon, float zoom) {
+    private void loadLocation(double lat, double lon, float zoom, String name) {
         LatLng loc = new LatLng(lat, lon);
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(loc, zoom);
         map.moveCamera(cameraUpdate);
+        map.addMarker(new MarkerOptions().position(loc).title(name));
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        loadLocation(9.1329, 7.3875945, 0); // add the car location here
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        map.setBuildingsEnabled(true);
+        map.getUiSettings().setCompassEnabled(true);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.getUiSettings().setRotateGesturesEnabled(true);
+        map.getUiSettings().setZoomControlsEnabled(true);
+        map.getUiSettings().setZoomGesturesEnabled(true);
+        loadLocation(9.1329, 7.3875945, 15, "Toyota"); // add the car location and name here
     }
 }
