6ffdb8134782513056190aec514f943fb40a66b9
chrissetiana <chrissetiana@gmail.com>
2018-09-13 00:55:25 +0100

    Check access permission

--- app/src/main/java/com/chrissetiana/wunderapp/MapActivity.java
+++ app/src/main/java/com/chrissetiana/wunderapp/MapActivity.java
@@ -1,11 +1,10 @@
 package com.chrissetiana.wunderapp;
 
+import android.Manifest;
 import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationManager;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -17,42 +16,19 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     GoogleMap map;
-    LocationManager locator;
-    Marker userMarker;
-    String name;
-    String address;
-    Double lat;
-    Double lon;
-
-    public MapActivity() {
-
-    }
-
-    public MapActivity(String name, double lat, double lon, String address) {
-        this.name = name;
-        this.lat = lat;
-        this.lon = lon;
-        this.address = address;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = getIntent();
-
         if (isPlayAvailable()) {
             setContentView(R.layout.activity_map);
             loadMap();
         }
-
-        myLocation();
     }
 
     public boolean isPlayAvailable() {
@@ -62,7 +38,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         if (available == ConnectionResult.SUCCESS) {
             return true;
         } else if (api.isUserResolvableError(available)) {
-            Dialog dialog = api.getErrorDialog(this, available, 10);
+            Dialog dialog = api.getErrorDialog(this, available, 15);
             dialog.show();
         } else {
             Toast.makeText(this, "Cannot connect to play services", Toast.LENGTH_LONG).show();
@@ -79,41 +55,30 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-
-        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        map.setBuildingsEnabled(true);
-        map.getUiSettings().setCompassEnabled(true);
-        map.getUiSettings().setMyLocationButtonEnabled(true);
-        map.getUiSettings().setRotateGesturesEnabled(true);
         map.getUiSettings().setZoomControlsEnabled(true);
-        map.getUiSettings().setZoomGesturesEnabled(true);
 
-        map.addMarker(loadMarker(lat, lon, name, address));
+        // goToLocation(53.551086, 9.993682, 15);
 
-        LatLng loc = new LatLng(53.551086, 9.993682);
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(loc, 15);
+        myLocation();
+    }
+
+    private void goToLocation(double lat, double lon, float zoom) {
+        LatLng loc = new LatLng(lat, lon);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(loc, zoom);
         map.moveCamera(cameraUpdate);
     }
 
-    private MarkerOptions loadMarker(Double lat, Double lon, String name, String desc) {
-        LatLng loc = new LatLng(lat, lon);
-        return new MarkerOptions().position(loc).title(name).snippet(desc);
-    }
-
     private void myLocation() {
-        locator = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Location location = locator.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-
-        double myLat = location.getLatitude();
-        double myLon = location.getLongitude();
-        LatLng myLoc = new LatLng(myLat, myLon);
-
-        if (userMarker != null) {
-            userMarker.remove();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
-
-        userMarker = map.addMarker(loadMarker(myLat, myLon, "You are here", "Last known location"));
-        // map.animateCamera(CameraUpdateFactory.newLatLng(myLoc), 3000, null);
+        map.setMyLocationEnabled(true);
     }
 }
